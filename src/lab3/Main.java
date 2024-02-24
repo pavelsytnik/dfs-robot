73e093b36b35dbff22d3c12fe8d7cea64db26e91
Pavel Sytnik <pashasytnik@gmail.com>
2024-02-24 16:31:57 +0400

    Apply changes in Main

--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -6,24 +6,11 @@ import lab3.env.*;
 public class Main {
     public static void main(String[] args) {
 
-        var map = new Map("res/map");
-        var robot = new Robot(map, new Pos(2, 5));
-        var flag = new Flag(map, new Pos(3, 1));
+        var map = new Map("res/maze");
+        var robot = new Robot(map, new Pos(4, 5));
+        var flag = new Flag(map, new Pos(3, 4));
 
-        for (int y = 0; y < map.getHeight(); y++) {
-            for (int x = 0; x < map.getWidth(); x++) {
-                var p = new Pos(x, y);
-                if (map.isWall(p)) {
-                    System.out.print("# ");
-                } else if (robot.getPos().equals(p)) {
-                    System.out.print("@ ");
-                } else if (flag.getPos().equals(p)) {
-                    System.out.print("$ ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
-        }
+        var dfs = new DeepFirstSearch(map, robot, flag);
+        dfs.search(robot.getPos());
     }
 }
