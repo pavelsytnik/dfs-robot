d68038acf1338100bef4c82b852ef6b6fede4136
Pavel Sytnik <pashasytnik@gmail.com>
2024-03-07 01:12:01 +0400

    Use changes in Main

--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -4,17 +4,40 @@ import lab3.util.Pos;
 import lab3.env.*;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
+        Scanner scan = new Scanner(System.in);
+
         var map = new Map("res/maze");
-        var robot = new Robot(map, new Pos(4, 5));
-        var flag = new Flag(map, new Pos(1, 1));
+        System.out.println("Map[" + map.getWidth() + "," + map.getHeight() + "]:\n");
+        map.print();
+        System.out.println();
 
-        Automaton.execute(robot, map, flag);
+        System.out.print("Enter start robot's coordinates: ");
+        int robotX = scan.nextInt();
+        int robotY = scan.nextInt();
 
-        var dfs = new DeepFirstSearch(map, robot, flag);
-        dfs.search(robot.getPos());
+        if (!map.isInsideMap(new Pos(robotX, robotY))) {
+            throw new RuntimeException("The robot went off the map");
+        }
+        var robot = new Robot(map, new Pos(robotX, robotY));
+
+        System.out.print("Enter flag's coordinates: ");
+        int flagX = scan.nextInt();
+        int flagY = scan.nextInt();
+
+        if (!map.isInsideMap(new Pos(flagX, flagY))) {
+            throw new RuntimeException("The flag is off the map");
+        }
+        var flag = new Flag(map, new Pos(flagX, flagY));
+
+        scan.close();
+
+        var automaton = new Automaton(map, robot, flag);
+        automaton.execute();
     }
 }
