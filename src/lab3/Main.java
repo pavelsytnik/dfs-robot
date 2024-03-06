9ebbd39c781b17278bd2f4dca218c5f19e9b34d0
Pavel Sytnik <pashasytnik@gmail.com>
2024-03-06 19:02:47 +0400

    Use new features in Main

--- src/lab3/Main.java
+++ src/lab3/Main.java
@@ -3,12 +3,16 @@ package lab3;
 import lab3.util.Pos;
 import lab3.env.*;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         var map = new Map("res/maze");
         var robot = new Robot(map, new Pos(4, 5));
-        var flag = new Flag(map, new Pos(3, 4));
+        var flag = new Flag(map, new Pos(1, 1));
+
+        Automaton.execute(robot, map, flag);
 
         var dfs = new DeepFirstSearch(map, robot, flag);
         dfs.search(robot.getPos());
